2f63170cb0fdb6135ff2566d08bf7aaf83b8be9b
satis <satis@SATSPPC>
2019-11-04 19:09:03 +0530

    test

---
+++ citi/src/main/java/AMEX/citi/App.java
@@ -0,0 +1,13 @@
+package AMEX.citi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" ); 
+    }
+}
